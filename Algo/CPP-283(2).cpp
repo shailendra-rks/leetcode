150c7f9b2b8b47695635894544a1882614cb6dde
shailendra-rks <shailendra.rks@gmail.com>
2022-12-30 22:58:02 +0530

    283. Move Zeroes. Optimal space, Optimal operation

---
+++ Algo/CPP-283(2).cpp
@@ -0,0 +1,9 @@
+// 283. Move Zeroes
+// Optimal space, Optimal operation
+void moveZeroes(vector<int>& nums) {
+    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+        if (nums[cur] != 0) {
+            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+        }
+    }
+}
