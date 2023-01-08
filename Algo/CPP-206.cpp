541c6dbb123fe896acddab4442bed4fc3625659c
shailendra-rks <shailendra.rks@gmail.com>
2023-01-08 16:10:33 +0530

    Reverse Linked List

---
+++ Algo/CPP-206.cpp
@@ -0,0 +1,31 @@
+// 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode* hi = head->next;
+        ListNode* lo = head;
+
+        while(hi != NULL){
+            head->next = hi->next;
+            hi->next = lo;
+            lo = hi;
+            hi = head->next;
+        }
+
+        return lo;
+    }
+};
